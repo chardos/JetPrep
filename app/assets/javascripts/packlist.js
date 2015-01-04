26103f097eae14d0c3c90f3bcf35a5d91ae508ee
Richard Tan <chardos@gmail.com>
2015-01-04 15:34:06 +1100

    completed editing functionality

--- app/assets/javascripts/packlist.js
+++ app/assets/javascripts/packlist.js
@@ -3,22 +3,35 @@ var J = J || {};
 J.list = {};
 
 J.list.toggleListItem = function(){
-  $('.pack-list').on('click', '.pack-list__item', function(e){
-    if ( !$(event.target).hasClass('pack-list__input') ){ //check its not the input
-      $(this).toggleClass('checked');
-    };
+
+  $('.pack-list').on('click', '.pack-list__checkbox', function(e){
+      $(this).parent().toggleClass('is-checked');
   });
 }
 
 J.list.editListItem = function(){
 
+  //open edit mode
   $('.pack-list').on('click', '.pack-list__edit', function(e){
-    $(this).parent().addClass('editing');
-    var labelVal = $(this).siblings('.pack-list__label')
-    $(this).siblings('.pack-list__input').val();
-    e.stopPropagation();
+    $(this).parent().addClass('is-editing');
+    var labelVal = $(this).siblings('.pack-list__label').html();
+    $(this).siblings('.pack-list__input').val(labelVal);
+    $(this).siblings('.pack-list__input').focus().select();
+
+  });
+
+  //close edit mode
+  $(document).on('click', function(e){
+    if ( $('.is-editing').length &&
+        !$(e.target).closest('.is-editing').length) 
+         //is-editing is taking place && click is not on the is-editing div 
+    { 
+      var textVal = $('.is-editing .pack-list__input').val();
+      $('.is-editing .pack-list__label').html(textVal);
+      $('.is-editing').removeClass('is-editing');
+    }
   })
-  
+
 }
 
 J.list.init = function(){
