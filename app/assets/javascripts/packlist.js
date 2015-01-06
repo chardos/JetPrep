80713a2f39efc60fdcaca61f1a71460a30c3d4df
Richard Tan <chardos@gmail.com>
2015-01-06 23:28:40 +1100

    Users session now saved in cookies

--- app/assets/javascripts/packlist.js
+++ app/assets/javascripts/packlist.js
@@ -6,6 +6,7 @@ J.list.toggleListItem = function(){
 
   $('.pack-list').on('click', '.pack-list__checkbox', function(e){
       $(this).parent().toggleClass('is-checked');
+      J.list.saveToCookies();
   });
 }
 
@@ -33,6 +34,7 @@ J.list.editListItemInit = function(){
     //if: is-editing is taking place && click is not on the is-editing div 
     { 
       J.list.closeEdit();
+      J.list.saveToCookies();
     }
   });
 
@@ -41,31 +43,72 @@ J.list.editListItemInit = function(){
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){
       J.list.closeEdit();
+      J.list.saveToCookies();
     }
   });
 }
 
-J.list.addListItem = function(){
+J.list.addListItemInit = function(){
   $('.js-add-item').on('click', function(){
     var el = '<li class="pack-list__item"><div class="pack-list__checkbox"></div><input class="pack-list__input" type="text"><span class="pack-list__label"></span><span class="pack-list__edit glyphicon glyphicon-pencil" aria-hidden="true"></span><span class="pack-list__delete glyphicon glyphicon-trash" aria-hidden="true"></span></li>'
     $('.pack-list').append(el);
     setTimeout(function(){
       J.list.openEdit( $('.pack-list__item:last').find('.pack-list__edit') );
     },50)
+    J.list.saveToCookies();
   });
 }
 
-J.list.deleteListItem = function(){
+J.list.deleteListItemInit = function(){
   $('.pack-list').on('click', '.pack-list__delete', function(e){
       $(this).parent().remove();
+      J.list.saveToCookies();
   });
+}
+
+J.list.saveToCookies = function(){
+  //save the data
+  var saveData = {};
+  $('.pack-list__item').each(function(index){
+    var label = $(this).find('.pack-list__label').html();
+    var checked = $(this).hasClass('is-checked');
+    saveData[index]={};
+    saveData[index].label = label; 
+    saveData[index].checked = checked; 
+  });
+  //set data in cookie
+  $.cookie("list", JSON.stringify( saveData ), { expires : 999 });
+}
+
+J.list.constructListItem = function(label, checked){
+  var $el = $('<li class="pack-list__item"></li>');
+  $el.append('<div class="pack-list__checkbox"></div>');
+  $el.append('<div class="pack-list__checkbox"></div>');
+  $el.append('<input class="pack-list__input" type="text">');
+  $el.append('<span class="pack-list__label">'+ label +'</span>');
+  $el.append('<span class="pack-list__edit glyphicon glyphicon-pencil" aria-hidden="true"></span>');
+  $el.append('<span class="pack-list__delete glyphicon glyphicon-trash" aria-hidden="true"></span>');
+  if(checked){ $el.addClass('is-checked') };
+  return $el;
+}
+
+J.list.retrieveFromCookies = function(){
+  var retrieved = JSON.parse($.cookie().list);
+  for (var key in retrieved) {
+    var obj = retrieved[key];
+    console.log(obj);
+    var $el = this.constructListItem( obj.label, obj.checked );
+    $('.pack-list').append($el)
+    
+  }
 }
 
 J.list.init = function(){
   this.toggleListItem();
   this.editListItemInit();
-  this.deleteListItem();
-  this.addListItem();
+  this.deleteListItemInit();
+  this.addListItemInit();
+  this.retrieveFromCookies();
 }
 
 $(document).ready(function(){
