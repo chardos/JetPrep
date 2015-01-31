cfd5b7e1b3c4dcfd3a1e7f5af81580db18e1053c
Richard Tan <chardos@gmail.com>
2015-02-01 10:23:26 +1100

    Added AJAX retrieve from DB.

--- app/controllers/pack_list_controller.rb
+++ app/controllers/pack_list_controller.rb
@@ -3,11 +3,14 @@ class PackListController < ApplicationController
   end
 
   def save_to_db
-    puts current_user.id
     p = PackList.find_by_user_id(current_user.id)
-    p.JSON = 'from controller noob'
+    p.JSON = params[:list]
     p.save
-    puts 'session[:user_id]'
     render text: "Updated details for: #{current_user.email}"
+  end
+
+  def retrieve_from_db
+    json = PackList.find_by_user_id(current_user.id).JSON
+    render text: json
   end
 end
