60fadc682f771f9c44ad18e06602d4bc42908912
Richard Tan <chardos@gmail.com>
2015-02-01 14:12:19 +1100

    Create new packlist and assign to each new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   has_one :pack_list
+
+  after_create :create_set_packlist
+
+  def create_set_packlist
+    
+    #create a packlist
+    pl = PackList.create()
+
+    #append to this user
+    self.pack_list = pl
+  end
 end
